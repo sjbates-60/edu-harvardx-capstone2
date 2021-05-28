3820690f61b541919588d4b53075b37256e53d21
sjbates-60 <68875794+sjbates-60@users.noreply.github.com>
2021-05-28 15:41:47 -0500

    Added code to compare the accuracies of several models on all features vs. a minimal set of features.

--- drybean.R
+++ drybean.R
@@ -8,6 +8,8 @@ require(readxl)
 require(log4r)
 require(RColorBrewer)
 
+options(digits = 5)
+
 ## Logging functions ------------------------------------------------------
 z__logger <- NULL
 
@@ -40,12 +42,16 @@ log_info <- function(msg) {
 
 
 ## 1. Load data --------------------------------------------------------------
+log_info("Dry Bean Dataset analysis")
+# log_info("Downloading data files...")
 # beanUrl <- "https://archive.ics.uci.edu/ml/machine-learning-databases/00602/DryBeanbeans.zip"
 # download.file(beanUrl, destfile = "bean.zip")
-# unzip("bean.zip", files = c("DryBeanbeans/Dry_Bean_beans.txt",
-#                             "DryBeanbeans/Dry_Bean_beans.xlsx"))
+# unzip("bean.zip", files = c("DryBeanDataset/Dry_Bean_Dataset.txt",
+#                             "DryBeanDataset/Dry_Bean_Dataset.xlsx"))
 # file.remove("bean.zip")
+# remove(beanUrl)
 
+log_info("Loading data...")
 beans <- read_excel("DryBeanDataset/Dry_Bean_Dataset.xlsx")
 beans <- beans %>% mutate(Class = factor(str_to_title(Class)))
 colnames(beans) <- c("A", "P", "X", "x", "K", "Ec", "C", "Ed", "Ex", "S", "R",
@@ -61,6 +67,8 @@ features <- setdiff(colnames(beans), "Class")
 #  Do this by showing the absolute value of the maximum difference is very
 #  close to zero.
 #
+log_info("Verifying mathematical relationships...")
+
 near_zero <- 10^-10
 stopifnot("Aspect ratio: K is not equal to X/x" =
             max(abs(beans$K - beans$X / beans$x)) < near_zero)
@@ -77,13 +85,59 @@ stopifnot("Roundness: R is not equal to 4*pi*A/P^2" =
 stopifnot("Compactness: CO is not equal to Ed/X" =
             max(abs(beans$CO - beans$Ed / beans$X)) < near_zero)
 
-# suppressWarnings(set.seed(16, sample.kind = "Rounding"))
-# test_index <- createDataPartition(beans$Class, times = 1, p = 0.1,
-#                                   list = FALSE)
-# training <- beans[-test_index, ]
-# test <- beans[test_index, ]
-# # Verify all classes are in both sets
-# stopifnot(unique(test$Class) == bean_types,
-#           unique(training$Class) == bean_types)
+## 3. Compare accuracy of several models--------------------------------------
 #
-# remove(beanUrl, test_index, beans)
+log_info("Creating training and testing sets...")
+suppressWarnings(set.seed(16, sample.kind = "Rounding"))
+test_index <- createDataPartition(beans$Class, times = 1, p = 0.1,
+                                  list = FALSE)
+training <- beans[-test_index, ]
+testing <- beans[test_index, ]
+# Verify all classes are in both sets
+stopifnot(unique(testing$Class) == bean_types,
+          unique(training$Class) == bean_types)
+
+remove(test_index, beans)
+
+# Trains a model on a set of features and reports the accuracy of the result.
+# Parameters:
+#   trainMethod  - the model to train.
+#   features     - a data frame containing the features.
+#   results      - a vector containing the classification results.
+#   trControl    - an optional training control method.
+#   tuneGrid     - optional tuning parameters.
+#
+compute_model_accuracy <- function(trainMethod, features, results,
+                                   trControl = trainControl(),
+                                   tuneGrid = NULL) {
+  log_info(sprintf("Checking model '%s' with features {%s}", trainMethod,
+                   paste(colnames(features), collapse = ", ")))
+
+  suppressWarnings(set.seed(16, sample.kind = "Rounding"))
+  suppressWarnings(fit <- train(features, results, method = trainMethod,
+                                trControl = trControl,
+                                tuneGrid = tuneGrid))
+  p <- predict(fit, newdata = features)
+  acc <- confusionMatrix(p, results)$overall[["Accuracy"]]
+  log_info(sprintf("  Accuracy: %f", acc))
+  acc
+}
+
+log_info("Computing the accuracy of several models...")
+
+features.min <- setdiff(features, c("K", "Ed", "R", "C", "CO", "S"))
+
+models <- c("naive_bayes", "lda", "qda", "knn", "kknn", "treebag")
+
+accuracies <- map_dfr(models, function(m) {
+  tibble(model = m,
+         acc.all = compute_model_accuracy(
+           m,
+           training %>% select(all_of(features)),
+           training$Class),
+         acc,min = compute_model_accuracy(
+           m,
+           training %>% select(all_of(features.min)),
+           training$Class))
+})
+saveRDS(accuracies, file == "accuracies.rds")
