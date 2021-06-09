ee83e51080a11c2069851b2745b640cda421cc69
sjbates-60 <68875794+sjbates-60@users.noreply.github.com>
2021-06-09 14:58:33 -0500

    Lots of changes: - Added utility functions. - Generalized computet_model_accuracy so that it can be used on the test set as well as the training set. - Added principal component analysis. - Got rid of two models. - Added the code to find the minimal set of features. - Added code to report which features are considered important in the treebag mdel, and calculated the accuracy using the ones that are at least 50% responsible for the result. - Added a stub for a section to compute model accuracies on the test set.

--- drybean.R
+++ drybean.R
@@ -1,5 +1,5 @@
 # Outline -----------------------------------------------------------------
-# Subject:
+# Subject: Selecting Features in a Dry Bean Dataset
 # Author: Samuel Bates
 #
 require(tidyverse)
@@ -9,6 +9,13 @@ require(log4r)
 require(RColorBrewer)
 
 options(digits = 5)
+
+## Utility functions ------------------------------------------------------
+# Converts a vector of strings into a string containing a comma-separated list.
+v2s <- function(v) paste(v, collapse = ",")
+
+# Converts a string containing a comma-separated list into a vector of strings.
+s2v <- function(s) strsplit(s, ",") %>% unlist()
 
 ## Logging functions ------------------------------------------------------
 z__logger <- NULL
@@ -57,11 +64,9 @@ beans <- beans %>% mutate(Class = factor(str_to_title(Class)))
 colnames(beans) <- c("A", "P", "X", "x", "K", "Ec", "C", "Ed", "Ex", "S", "R",
                      "CO", "SF1", "SF2", "SF3", "SF4", "Class")
 bean_types <- unique(beans$Class)
-bean_proportions <- beans %>%
-  group_by(Class) %>%
-  summarize(n = n(), p = n() / nrow(beans))
 
 features <- setdiff(colnames(beans), "Class")
+
 
 ## 2. Verify mathematical relationships among features------------------------
 #  Do this by showing the absolute value of the maximum difference is very
@@ -85,7 +90,8 @@ stopifnot("Roundness: R is not equal to 4*pi*A/P^2" =
 stopifnot("Compactness: CO is not equal to Ed/X" =
             max(abs(beans$CO - beans$Ed / beans$X)) < near_zero)
 
-## 3. Compare accuracy of several models--------------------------------------
+
+## 3. Compare the accuracy of several models----------------------------------
 #
 log_info("Creating training and testing sets...")
 suppressWarnings(set.seed(16, sample.kind = "Rounding"))
@@ -99,45 +105,180 @@ stopifnot(unique(testing$Class) == bean_types,
 
 remove(test_index, beans)
 
-# Trains a model on a set of features and reports the accuracy of the result.
+# Trains a model on a set of features and reports its accuracy on a different
+# set of features.
 # Parameters:
 #   trainMethod  - the model to train.
-#   features     - a data frame containing the features.
-#   results      - a vector containing the classification results.
-#   trControl    - an optional training control method.
-#   tuneGrid     - optional tuning parameters.
+#   features.tr  - a data frame containing the features for training.
+#   features.pr  - a data frame containing the features for predicting.
+#   results      - a vector containing the actual values on features.pr.
+# Returns:
+#   The accuracy of the model's predictions.
 #
-compute_model_accuracy <- function(trainMethod, features, results,
-                                   trControl = trainControl(),
-                                   tuneGrid = NULL) {
+compute_model_accuracy <- function(trainMethod, features.tr, features.pr,
+                                   results) {
   log_info(sprintf("Checking model '%s' with features {%s}", trainMethod,
-                   paste(colnames(features), collapse = ", ")))
+                   v2s(colnames(features.tr))))
 
   suppressWarnings(set.seed(16, sample.kind = "Rounding"))
-  suppressWarnings(fit <- train(features, results, method = trainMethod,
-                                trControl = trControl,
-                                tuneGrid = tuneGrid))
-  p <- predict(fit, newdata = features)
+  fit <- suppressWarnings(train(features, results, method = trainMethod))
+  p <- predict(fit, newdata = features.pr)
   acc <- confusionMatrix(p, results)$overall[["Accuracy"]]
   log_info(sprintf("  Accuracy: %f", acc))
   acc
 }
 
-log_info("Computing the accuracy of several models...")
+log_info("Performing principal components analysis...")
+
+pca <- prcomp(training %>% select(-Class))
+
+log_info("Computing the accuracy of several models on multiple feature sets...")
 
 features.min <- setdiff(features, c("K", "Ed", "R", "C", "CO", "S"))
 
-models <- c("naive_bayes", "lda", "qda", "knn", "kknn", "treebag")
+models <- c("lda", "qda", "knn", "treebag")
 
 accuracies <- map_dfr(models, function(m) {
   tibble(model = m,
          acc.all = compute_model_accuracy(
            m,
            training %>% select(all_of(features)),
+           training %>% select(all_of(features)),
            training$Class),
-         acc,min = compute_model_accuracy(
+         acc.min = compute_model_accuracy(
            m,
            training %>% select(all_of(features.min)),
+           training %>% select(all_of(features.min)),
+           training$Class),
+         acc.pca2 = compute_model_accuracy(
+           m,
+           pca$x[, 1:2],
+           pca$x[, 1:2],
+           training$Class),
+         acc.pca6 = compute_model_accuracy(
+           m,
+           pca$x[, 1:6],
+           pca$x[, 1:6],
            training$Class))
 })
-saveRDS(accuracies, file == "accuracies.rds")
+saveRDS(accuracies, file = "training_accuracies.rds")
+
+
+## 4. Discover a minimal set of features that maximizes accuracy--------------
+#
+
+# Finds features that maximize the accuracy of a model when added to an
+# existing set of features. This is a recursive function that halts either
+# when there are no more features to add, or when no feature increases the
+# accuracy of the model.
+#
+# Parameters:
+#   current     - The current state, containing:
+#                   model - The model being trained.
+#                   fset  - A comma-separated list of features.
+#                   acc   - The accuracy of the model on fset.
+#   feature_set - The complete set of features that can be used.
+# Returns:
+#   A final state, containing model, fset, and acc, such that no feature
+#   in feature_set will increase the accuracy of the model.
+#
+fewest_features <- function(current, feature_set) {
+  # Get the lists of used and unused features.
+  used <- s2v(current$fset)
+  unused <- setdiff(feature_set, used)
+
+  if (length(unused) == 0)
+    # In this case, there are no more features to try,
+    # so we return the current result.
+    current
+  else {
+    # Loop through the unused features, calculating the accuracy
+    # achieved when adding each to the used features.
+    accs <- sapply(unused, function(f) {
+      compute_model_accuracy(current$model,
+                             training %>% select(all_of(c(used, f))),
+                             training %>% select(all_of(c(used, f))),
+                             training$Class)
+    })
+    # Get the highest accuracy value.
+    ind <- which.max(accs)
+    if (accs[ind] <= current$acc)
+      # In this case, none of the new features yields a higher accuracy
+      # than what we already have, so we return the current result.
+      current
+    else {
+      # In this case, at least one feature yields a higher accuracy value.
+      # Take the one that maximizes the accuracy, add it to the set of
+      # used features, and call the function again with the new accuracy
+      # and used feature set.
+      fset2 <- v2s(c(used, unused[ind]))
+      fewest_features(tibble(model = current$model,
+                             acc = accs[ind],
+                             fset = fset2),
+                      feature_set)
+    }
+  }
+}
+
+options(pillar.sigfig = 6)
+
+# For each model, start with the empty set of features and find a minimal
+# set of features that maximizes accuracy when all features are available.
+model_results <- map_dfr(models, function(m) {
+  bestfit <- fewest_features(tibble(model = m, acc = 0.0, fset = ""), features)
+  log_info(sprintf("All features: Fewest for '%s' is {%s} with accuracy %f.",
+                   bestfit$model, bestfit$fset, bestfit$acc))
+  log_info("")
+  bestfit
+})
+saveRDS(model_results, file = "fewest_all.rds")
+
+# For each model, start with the empty set of features and find a minimal
+# set of features that maximizes accuracy when only the mathemtically
+# independent features are available.
+model_results <- map_dfr(models, function(m) {
+  bestfit <- fewest_features(tibble(model = m, acc = 0.0, fset = ""),
+                             features.min)
+  log_info(sprintf("Minimum features: Fewest for '%s' is {%s} with accuracy %f.",
+                   bestfit$model, bestfit$fset, bestfit$acc))
+  log_info("")
+  bestfit
+})
+saveRDS(model_results, file = "fewest_min.rds")
+
+
+## 5. Compare the minimal sets to treebag's own reporting of importance-------
+#
+
+log_info(sprintf("Training model 'treebag' on {%s}...", v2s(features)))
+suppressWarnings(set.seed(16, sample.kind = "Rounding"))
+fit_all <- suppressWarnings(train(training %>% select(all_of(features)),
+                                  training$Class,
+                                  method = "treebag"))
+varImp_all <- varImp(fit_all)$importance
+mostImp_all <- varImp_all %>% filter(Overall > 50) %>% rowNames()
+log_info(sprintf("Most important features are {%s}.", v2s(mostImp_all)))
+acc.mostImp_all <- compute_model_accuracy(
+  "treebag",
+  training %>% select(all_of(mostImp_all)),
+  training %>% select(all_of(mostImp_all)),
+  training$Class)
+
+log_info(sprintf("Training model 'treebag' on {%s}...", v2s(features.min)))
+suppressWarnings(set.seed(16, sample.kind = "Rounding"))
+fit_min <- suppressWarnings(train(training %>% select(all_of(features.min)),
+                                  training$Class,
+                                  method = "treebag"))
+varImp_min <- varImp(fit_min)$importance
+mostImp_min <- varImp_min %>% filter(Overall > 50) %>% rowNames()
+log_info(sprintf("Most important features are {%s}.", v2s(mostImp_min)))
+acc.mostImp_min <- compute_model_accuracy(
+  "treebag",
+  training %>% select(all_of(mostImp_min)),
+  training %>% select(all_of(mostImp_min)),
+  training$Class)
+
+
+## 6. Calculate accuracies of the various models on the test set--------------
+#
+
